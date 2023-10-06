4d748e33fb09d2ffd472765e28010a6a400f7bc0
Brixter Porras <brixterporras@gmail.com>
2023-10-06 18:05:39 +0800

    chore: format structu

--- internal/domain/meta.go
+++ internal/domain/meta.go
@@ -1,24 +1,23 @@
 package domain
 
 type MetaData struct {
-	Page       int `json:"page" example:"1"`
+	Page       int `json:"page"       example:"1"`
 	TotalPages int `json:"totalPages" example:"10"`
-	PerPage    int `json:"perPage" example:"1000"`
+	PerPage    int `json:"perPage"    example:"1000"`
 	TotalItems int `json:"totalItems" example:"10000"`
-	ItemCount  int `json:"itemCount" example:"1000"`
+	ItemCount  int `json:"itemCount"  example:"1000"`
 } //@name MetaData
 //? comment above is for renaming stuct
 
-
 type PaginatedResponse struct {
-	MetaData MetaData                   `json:"metadata"`
+	MetaData MetaData     `json:"metadata"`
 	Data     []Masterlist `json:"data"`
 } //@name PaginatedResponse
 //? comment above is for renaming stuct
 
 type PaginationParams struct {
-	Page    int    `json:"page" example:"1"`
+	Page    int    `json:"page"    example:"1"`
 	PerPage int    `json:"perPage" example:"1000"`
-	Filter  string `json:"filter" example:"filter"` // This is filter for all the field in the object GeographicArea
+	Filter  string `json:"filter"  example:"filter"` // This is filter for all the field in the Masterlist object
 } //@name PaginationParams
-//? comment above is for renaming stuct
+// INFO? comment above is for renaming stuct
